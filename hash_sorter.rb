9e6a6a8d6f82c629ab9d691dcd4a0054b069aa0a
kazuhisa <ak.hisashi@gmail.com>
2015-01-30 17:57:14 +0900

    add sort logic.

--- hash_sorter.rb
+++ hash_sorter.rb
@@ -1,6 +1,19 @@
 require 'ya2yaml'
 require 'yaml'
 
+class Hash
+  # http://dan.doezema.com/2012/04/recursively-sort-ruby-hash-by-key/
+  def sort_by_key(recursive = false, &block)
+    self.keys.sort(&block).reduce({}) do |seed, key|
+      seed[key] = self[key]
+      if recursive && seed[key].is_a?(Hash)
+        seed[key] = seed[key].sort_by_key(true, &block)
+      end
+      seed
+    end
+  end
+end
+
 path = ARGV[0]
 h = YAML.load_file(path)
-puts h.ya2yaml
+puts h.sort_by_key.ya2yaml.gsub(/\s$/,"\n")
